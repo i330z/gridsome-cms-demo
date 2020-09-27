201c96723a68b1cd70e2aa662ed96de9766ba3f8
i330z <izzazahmed134@gmail.com>
2020-09-27 21:39:41 +0530

    price table added to product page

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,37 +1,65 @@
 <template>
-    <Layout>
-
-        <section>
-            <b-row>
-                <b-col md="12">
-                    <div style="text-align:center;padding: 90px 0px; background: lightgrey">
-                        <h2>Chocolate Cake</h2>
-                    </div>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col md="6" class="py-100 mt-5" style="background: lightyellow; border-radius: 12px">
-                    <img :src="$page.post.thumbnail" class="detail-page-img" alt="" />
-                </b-col>
-                <b-col md="6" class="py-100 mt-5 px-5"> 
-                    <h1>{{ $page.post.title }}</h1>
-                    <span style="color:#FF599A; font-weight: bold; font-size: 25px">&#8377; 300</span>
-                    <!-- <div v-html="$page.post.content" class="mt-3"></div> -->
-                    <p class="mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi inventore ea sint, ullam assumenda voluptates doloremque deleniti dolores, fugit nam autem! Dolorum voluptatum consectetur illum reprehenderit deserunt numquam officia autem.</p>
-                    <div class="cake-detail">
-                        <span><strong>Flavour:</strong> Chocolate</span> <br>
-                        <span><strong>Price:</strong></span>
-                    </div>
-                </b-col>
-            </b-row>
-        </section>
-        <!-- <h1>{{ $page.post.title }}</h1>
+  <Layout>
+    <section>
+      <b-row>
+        <b-col md="12">
+          <div
+            style="text-align:center;padding: 90px 0px; background: lightgrey"
+          >
+            <h2>Chocolate Cake</h2>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col
+          md="6"
+          class="py-100 mt-4"
+          style="background: lightyellow; border-radius: 12px"
+        >
+          <img :src="$page.post.thumbnail" class="detail-page-img" alt="" />
+        </b-col>
+        <b-col md="6" class="py-100 mt-0 px-5">
+          <h1>{{ $page.post.title }}</h1>
+          <span style="color:#FF599A; font-weight: bold; font-size: 25px"
+            >&#8377; 300</span
+          >
+          (0.5kg)
+          <!-- <div v-html="$page.post.content" class="mt-3"></div> -->
+          <p class="mt-4">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi
+            inventore ea sint, ullam assumenda voluptates doloremque deleniti
+            dolores, fugit nam autem! Dolorum voluptatum consectetur illum
+            reprehenderit deserunt numquam officia autem.
+          </p>
+          <div class="cake-detail">
+            <span><strong>Flavour:</strong> Chocolate</span> <br />
+            <span><strong>Price:</strong></span>
+            <table>
+              <tr>
+                <th>Kg</th>
+                <th>price</th>
+              </tr>
+              <tr>
+                <td>0.5kg</td>
+                <td>&#8377; 300</td>
+              </tr>
+              <tr>
+                <td>0.5kg</td>
+                <td>&#8377; 500</td>
+              </tr>
+            </table>
+          </div>
+          <div class="mt-4 text-center">
+            <button class="card-button">Order Now</button>
+          </div>
+        </b-col>
+      </b-row>
+    </section>
+    <!-- <h1>{{ $page.post.title }}</h1>
         <img :src="$page.post.thumbnail" width="500" alt="">
         <div v-html="$page.post.content"></div> -->
-    </Layout>
+  </Layout>
 </template>
-
-
 
 <page-query>
 
@@ -47,18 +75,24 @@ query Post($path: String !){
 </page-query>
 
 <style scoped>
-.cake-detail span{
-    /* margin-bottom: 1px; */
-    display: block;
+.cake-detail span {
+  /* margin-bottom: 1px; */
+  display: block;
 }
 
-.cake-detail span>strong{
-    font-size: 20px;
+.cake-detail span > strong {
+  font-size: 20px;
 }
 
-.detail-page-img{
-    width: 100%;
-    
+.detail-page-img {
+  width: 100%;
+}
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 8px;
+  width: 20%;
 }
 </style>
-
